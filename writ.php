3afceae8e60bf4d4ae4692da3c019ea7309eb45c
Jourando <Jourando@users.noreply.github.com>
2017-08-30 11:18:52 +0300

    writ php test [2]

--- writ.php
+++ writ.php
@@ -6,8 +6,19 @@ list($d, $act, $oldStr, $newStr) = explode("##", $tmp1);
 // fclose($handler);
 $xfile = 'depart'.$d.'.js';
 $lines = file($xfile, FILE_IGNORE_NEW_LINES | FILE_SKIP_EMPTY_LINES);
+$handle = fopen($xfile, 'w');
 $i=0;
 foreach($lines as $v) {
-		echo $v."<br>";
+		// echo $v."<br>";
+		echo $lines[$i]."<br>";
+		if (strtoupper($act)=="REPLACE") {
+			if ($lines[$i] == $oldStr) {
+				fwrite($handle, $newStr);
+			} else {
+				fwrite($handle, $lines[$i]);
+			}
+		}
+		$i++;
 }
+fclose($handle);
 ?>
