89a92fc44557fd8f2010fd3978a92644272034fc
Jourando <Jourando@users.noreply.github.com>
2017-08-31 02:58:03 +0300

    Try to send Del, Before and After [8]
    some bugz are still here

--- writ.php
+++ writ.php
@@ -5,14 +5,13 @@ $tmp1=$_POST['param'];
 list($d, $act, $oldStr, $newStr) = explode("##", $tmp1);
 $xfile = 'depart'.$d.'.js';
 $lines = file($xfile, FILE_IGNORE_NEW_LINES | FILE_SKIP_EMPTY_LINES);
-// 0744
+// chmod 0744
 if (!file_exists('oldata/'.$d)) { mkdir('oldata/'.$d, 0744, true); }
 $newfile='oldata/'.$d.'/depart'.$d;
 $f = scandir('oldata/'.$d);
 $j=count($f)-1;
 $newfile=$newfile."[".$j."]";
 copy($xfile, $newfile);
-// $handle = fopen($xfile, 'w');
 $i=0;
 $j=$i;
 $act=strtoupper($act);
@@ -20,28 +19,11 @@ foreach($lines as $v) {
 		echo $act."<br>".$oldStr."<br>".$newStr."<br>";
 		if ($act=="REPLACE") {
 			if ($lines[$i] == "d".$d."[".$i."]='".$oldStr."';") {
-				// fwrite($handle, "d".$d."[".$i."]='".$newStr."';\r\n");
 				$aStr[] = "d".$d."[".$i."]='".$newStr."';\r\n";
 			} else {
-				// fwrite($handle, $lines[$i]."\r\n");
 				$aStr[] = $lines[$i]."\r\n";
 			}
 		}
-/*		if (($act=="BEFORE") || ($act=="AFTER")) {
-			if ($lines[$i] == "d".$d."[".$j."]='".$oldStr."';") {
-					if ($act=="BEFORE") {
-						fwrite($handle, "d".$d."[".$j."]='".$newStr."';\r\n");
-						fwrite($handle, "d".$d."[".($j+1)."]='".$oldStr."';\r\n");
-					} else {
-						fwrite($handle, "d".$d."[".$j."]='".$oldStr."';\r\n");
-						fwrite($handle, "d".$d."[".($j+1)."]='".$newStr."';\r\n");
-					}
-					$j=$j+1;
-			} else {
-				fwrite($handle, $lines[$i]."\r\n");
-			}			
-		}
-*/
 		if ($act=="BEFORE") {
 			if ($lines[$i] == "d".$d."[".$i."]='".$oldStr."';") {
 				$aStr[]="d".$d."[".$j."]='".$newStr."';\r\n";
@@ -77,7 +59,6 @@ foreach($lines as $v) {
 		$i=$i+1;
 		$j=$j+1;
 }
-// fclose($handle);
 $handle = fopen($xfile, 'w');
 for ($i=0; $i<count($aStr); $i++) {
 	fwrite($handle, $aStr[$i]);
