a5ab79bed0ce3524300acd06817eea829b0a1ee0
Jourando <Jourando@users.noreply.github.com>
2017-08-30 03:37:12 +0300

    Delete index.php

--- index.php
+++
@@ -1,6 +0,0 @@
-<?php
-echo "<html><body>";
-echo "<table width=100% height=100%>\n<tr><td align=center>\n<h2 align=center>That is /home/test2.ru/www/index.php.</h2>\n</td></tr>\n</table>\n";
-echo "<p>Just "."test</p>";
-echo "</body></html>";
-?>
