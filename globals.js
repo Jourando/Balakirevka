b155d844be22e68641877a086535db5b93b7a18e
Jourando <Jourando@users.noreply.github.com>
2017-08-30 12:18:45 +0300

    writ php test [6] + Archive
    old vers were moved to OLD dir

--- globals.js
+++ globals.js
@@ -1,2 +1,3 @@
 urlStr='http://test2.ru/auth.php?';
 datStr='http://test2.ru/writ.php?';
+// baseUrl = 'http:/'+'/test2.ru/';
--- writ.php
+++ writ.php
@@ -6,9 +6,9 @@ $lines = file($xfile, FILE_IGNORE_NEW_LINES | FILE_SKIP_EMPTY_LINES);
 $handle = fopen($xfile, 'w');
 $i=0;
 foreach($lines as $v) {
-		echo $lines[$i]."<br>";
+		// echo $lines[$i]."<br>";
 		if (strtoupper($act)=="REPLACE") {
-			if ($lines[$i] == $oldStr) {
+			if ($lines[$i] == "d".$d."[".$i."]='".$oldStr."';") {
 				fwrite($handle, "d".$d."[".$i."]='".$newStr."';\r\n");
 			} else {
 				fwrite($handle, $lines[$i]."\r\n");
