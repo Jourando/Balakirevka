a35d67ed375cecdcb81fbfed17b082b3f5b1df05
Jourando <Jourando@users.noreply.github.com>
2017-09-15 15:23:19 +0300

    Remake main 2 php

---
+++ globals2.js
@@ -0,0 +1,5 @@
+﻿rd='sh_tab_xls_v';
+urlStr='http://test2.ru/auth.php?';
+datStr='http://test2.ru/writ.php?';
+depMod=0;
+curMod=0;
--- sh_tab_xls_v9.php
+++ sh_tab_xls_v9.php
@@ -13,6 +13,7 @@ if (ISSET($_GET['us'])==true) {
 <html lang="ru">
 <head>
 <meta http-equiv="X-UA-Compatible" content="IE=edge"><meta charset="utf-8">
+<Title>Main Tab Editor</Title>
 <style>
 body {margin: 0px; padding: 0px}
 th {border: 1px solid #000; background: silver}
@@ -30,20 +31,55 @@ input[type=text] {resize:both;}
 .visible {display: inline-block;}
 .invisible {display: none;}
 </style>
-<!--
-<script src=depart0.js?rev=123></script>
-<script src=depart1.js?rev=123></script>
-<script src=depart2.js?rev=123></script>
-<script src=depart3.js?rev=123></script>
--->
 </head>
 <body id=mBody>
-<script src=globals.js?rev=123></script>
+<script src=globals2.js?rev=123></script>
+<?
+function mkMenu() {
+$xfile="depart0.a";
+$lines = file($xfile, FILE_IGNORE_NEW_LINES | FILE_SKIP_EMPTY_LINES);
+$i=0;
+$defStr='1||||||||||||||||';
+echo "<form autocomplete=1><fieldset name=set id=fset>\r\n";
+echo "<label>Отдел <select>\r\n";
+foreach($lines as $v) {
+	list($mVal[$i], $mLable[$i])=explode("=", $lines[$i]);
+	if ($i>0) {
+		$resStr[$i]="<option value=".$mVal[$i].">[".$mVal[$i]."] ".$mLable[$i]."</option>\r\n";
+	} else {
+		$resStr[$i]="<option value=".$mVal[$i].">".$mLable[$i]."</option>\r\n";
+	}
+	echo $resStr[$i];
+	if (!FILE_EXISTS('depart'.$i.'.a')) {
+		$handle = fopen('depart'.$i.'.a', 'w');
+		fwrite($handle, $defStr."\r\n");
+		fclose($handle);
+	}
+	$i=$i+1;
+}
+echo "</select></label>\r\n";
+echo "<label> оператор <input type=text id=lusr value=\"Фамилия и инициалы\" OnFocus=this.value=\"\" OnBlur=\"if (this.value=='') {this.value='Фамилия и инициалы';}\"> пароль <input id=pusr type=password value=Пароль OnFocus=this.value=\"\" OnBlur=\"if (this.value=='') {this.value='Пароль';}\"> <input type=button value=Отправить Onclick=f8() class=visible id=b1> <input type=button value=Перелогиниться Onclick=f9() class=invisible id=b2> <input type=button value=Обновить OnClick=\"location.reload()\"></label>\r\n";
+echo "</fieldset></form>\r\n";
+}
+function getContent($a1) {
+// $dir = __DIR__;
+// $f=scandir($dir);
+foreach (glob("depart*.a") as $filename) {
+    echo "$filename размер " . filesize($filename) . "<br>\r\n";
+}
+}
+?>
 <DIV id=mainSection>
-<Script>
-document.write('<form autocomplete=1><fieldset name="set" id=fset></fieldset></form>');
-document.write('<Table width=100% border=0 id=mainTab>'+hdrStr+'</Table>');
-</Script>
+<? mkMenu() ?>
+<Table width=100% border=0 id=mainTab>
+<tr>
+	<th rowspan=2>номер</th><th rowspan=2>дата</th><th rowspan=2>вид деятельности</th><th colspan=3>мероприятие</th><th rowspan=2>место проведения</th><th colspan=4>охват</th><th colspan=3>проводящие</th><th rowspan=2>организационно-<br>финансовое</th><th rowspan=2>доп.<br>информация</th>
+</tr><tr>
+	<th>тип</th><th>наши/сторонние</th><th>название</th><th>тип</th><th>целевая аудитория</th><th>зрители</th><th>выступающие/участники</th><th>отделение</th><th>нач.отделения</th><th>ответственный</th>
+</tr>
+
+</Table>
+<? getContent('all') ?>
 </DIV>
 
 </body>
