3cf384cff2a6451c89fe5adb72439cc4519fe97a
Jourando <Jourando@users.noreply.github.com>
2017-09-15 17:03:59 +0300

    Remake main 2 php-2

--- sh_tab_xls_v9.php
+++ sh_tab_xls_v9.php
@@ -60,26 +60,42 @@ foreach($lines as $v) {
 echo "</select></label>\r\n";
 echo "<label> оператор <input type=text id=lusr value=\"Фамилия и инициалы\" OnFocus=this.value=\"\" OnBlur=\"if (this.value=='') {this.value='Фамилия и инициалы';}\"> пароль <input id=pusr type=password value=Пароль OnFocus=this.value=\"\" OnBlur=\"if (this.value=='') {this.value='Пароль';}\"> <input type=button value=Отправить Onclick=f8() class=visible id=b1> <input type=button value=Перелогиниться Onclick=f9() class=invisible id=b2> <input type=button value=Обновить OnClick=\"location.reload()\"></label>\r\n";
 echo "</fieldset></form>\r\n";
+return $mLable;
 }
-function getContent($a1) {
+function getContent($a1, $m1) {
 // $dir = __DIR__;
 // $f=scandir($dir);
+$i=0;
 foreach (glob("depart*.a") as $filename) {
-    echo "$filename размер " . filesize($filename) . "<br>\r\n";
+    $fArray[$i]=$filename;
+	$lines[$i] = file($fArray[$i], FILE_IGNORE_NEW_LINES | FILE_SKIP_EMPTY_LINES);
+	$j=0;
+	if ($fArray[$i] !== "depart0.a") {
+		$rx[$i]="<tr id=sec".$i."hdr class=T2><td id=hdr".$i." colspan=16 class=depHdr>Отдел: ".$m1[$i]."</td></tr>\r\n";
+		echo $rx[$i];
+		foreach($lines[$i] as $v) {
+			list($n, $dt, $vd, $acType, $acOwner, $acName, $acPlace, $oType, $oAud, $oSeer, $oPrt, $hostDep, $hostHead, $hostLd, $fin, $adInfo) = explode("|", $lines[$i][$j]);
+			$rs[$i][$j]="<tr id=sec".$i."line".$j." class=T1><td id=s".$i."r".$j."c1>".$n."</td><td id=s".$i."r".$j."c2>".$dt."</td><td id=s".$i."r".$j."c3>".$vd."</td><td id=s".$i."r".$j."c4>".$acType."</td><td id=s".$i."r".$j."c5>".$acOwner."</td><td id=s".$i."r".$j."c6>".$acName."</td><td id=s".$i."r".$j."c7>".$acPlace."</td><td id=s".$i."r".$j."c8>".$oType."</td><td id=s".$i."r".$j."c9>".$oAud."</td><td id=s".$i."r".$j."c10>".$oSeer."</td>";
+			$rs[$i][$j]=$rs[$i][$j]."<td id=s".$i."r".$j."c11>".$oPrt."</td><td id=s".$i."r".$j."c12>".$hostDep."</td><td id=s".$i."r".$j."c13>".$hostHead."</td><td id=s".$i."r".$j."c14>".$hostLd."</td><td id=s".$i."r".$j."c15>".$fin."</td><td id=s".$i."r".$j."c16>".$adInfo."</td></tr>\r\n";
+			echo $rs[$i][$j];
+			$j=$j+1;
+		}
+	}
+	$i=$i+1;
 }
+// print_r($m1);
 }
 ?>
 <DIV id=mainSection>
-<? mkMenu() ?>
+<? $tA=mkMenu() ?>
 <Table width=100% border=0 id=mainTab>
 <tr>
 	<th rowspan=2>номер</th><th rowspan=2>дата</th><th rowspan=2>вид деятельности</th><th colspan=3>мероприятие</th><th rowspan=2>место проведения</th><th colspan=4>охват</th><th colspan=3>проводящие</th><th rowspan=2>организационно-<br>финансовое</th><th rowspan=2>доп.<br>информация</th>
 </tr><tr>
-	<th>тип</th><th>наши/сторонние</th><th>название</th><th>тип</th><th>целевая аудитория</th><th>зрители</th><th>выступающие/участники</th><th>отделение</th><th>нач.отделения</th><th>ответственный</th>
+	<th>тип</th><th>внутренние/сторонние</th><th>название</th><th>тип</th><th>целевая аудитория</th><th>зрители</th><th>выступающие/участники</th><th>отделение</th><th>нач.отделения</th><th>ответственный</th>
 </tr>
-
+<? getContent('all', $tA) ?>
 </Table>
-<? getContent('all') ?>
 </DIV>
 
 </body>
