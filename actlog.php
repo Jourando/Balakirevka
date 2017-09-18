240d533e66d25361b9307659d140671985a70e65
Jourando <Jourando@users.noreply.github.com>
2017-09-18 05:29:47 +0300

    making actlog 2

--- actlog.php
+++ actlog.php
@@ -1,6 +1,8 @@
 <?php
 // actlog
 // формат: Время, юзер, с какой стр., c какого объекта, какое действие, тип, успех или код ошибки
+// формат: Ф-ция, имя переменной, знач. переменной
+$a="";
 $xfile = 'actlog.txt';
 $usr = 'guest';
 $pg = '-';
@@ -8,7 +10,10 @@ $obj = '-';
 $exc = '-';
 $type = 'ajax';
 $code = '0';
-if ($_GET['act']=='WL') {
+$fnc = '-';
+$u_var = 'undefined';
+$u_val = 'undefined';
+if ($_GET['act']=='ACTL') {
 	$tm = date("d m Y H:i:s");
 	if (ISSET($_GET['u'])) {
 		$usr = $_GET['u'];
@@ -28,9 +33,24 @@ if ($_GET['act']=='WL') {
 	if (ISSET($_GET['c'])) {
 		$code = $_GET['c'];
 	}
-	$a = $usr." @ ".$tm." from ".$pg." : action ".$exc." on obj ".$obj." via ".$type."... Result is ".$code."\r\n";
+	$a = $usr." @ ".$tm." from ".$pg." : action ".$exc." on obj ".$obj." via ".$type."... Result code is ".$code."\r\n";
 	$handle = fopen($xfile, 'a');
 	fwrite($handle, $a);
 	fclose($handle);
 }
+if ($_GET['act']=='VARL') {
+	if (ISSET($_GET['f'])) {
+		$fnc = $_GET['f'];
+	}
+	if (ISSET($_GET['v1'])) {
+		$u_var = $_GET['v1'];
+	}
+	if (ISSET($_GET['v2'])) {
+		$u_val = $_GET['v2'];
+	}
+	$a = "{variable ".$u_var."=".$u_val." --- from function ".$fnc."}\r\n";
+}
+$handle = fopen($xfile, 'a');
+fwrite($handle, $a);
+fclose($handle);
 ?>
