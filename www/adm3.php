66ef16a1d10fad264b507270234e0ed10212b29e
Paul the Stranger <paul.ivan.stranger@gmail.com>
2017-09-21 02:38:28 +0300

    adm tools 6

--- www/adm3.php
+++ www/adm3.php
@@ -1,7 +1,7 @@
 <?php
 echo "<html><head><title>adm tools</title></head><body>";
 if ((!ISSET($_POST['log'])) || (!ISSET($_POST['dep'])) || (ISSET($_POST['relog']))) {
-	$str1='<h3>adm tools</h3><form action="" target=_blank method=post><input type=text value=0 name="dep"> : <input type=text value="Input password" name="log">';
+	$str1='<h3>adm tools</h3><form action="" target=_self method=post><input type=text value=0 name="dep"> : <input type=text value="Input password" name="log">';
 	$str2='<input type=submit value="Send"></form>';
 	echo $str1.$str2;
 } else {
@@ -17,7 +17,7 @@ if ((!ISSET($_POST['log'])) || (!ISSET($_POST['dep'])) || (ISSET($_POST['relog']
 		} else {
 			$str1='<h3>adm tools</h3><form action="" target=_self method=post>Access blocked <input type=hidden value=0 name="relog">';
 			$str2='<input type=submit value="Retry"></form>';
-			// echo 'фейл: '.$i.'.'.$_POST['log'].' - ['.$login[$i][0].' @ 999] and ['.md5($_POST['log']).' @ '.$login[$i][2].']<br>';
+			echo '<!-- фейл: '.$i.'.'.$_POST['log'].' - ['.$login[$i][0].' @ 999] and ['.md5($_POST['log']).' @ '.$login[$i][2].'] --><br>'\r\n;
 		}
 	}
 	echo $str1.$str2;
