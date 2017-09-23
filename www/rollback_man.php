a3e2fef8270b53ff4fb392dc883b4c9e5aa11101
Paul the Stranger <paul.ivan.stranger@gmail.com>
2017-09-23 21:15:23 +0300

    adm tools 29

--- www/rollback_man.php
+++ www/rollback_man.php
@@ -19,7 +19,8 @@ function xlsWriteLabel($Row, $Col, $Value ) {
     return;
 }
 function chCode($inStr) {
-return mb_convert_encoding($inStr,"UTF-8","Windows-1251");	
+// return mb_convert_encoding($inStr,"UTF-8","Windows-1251");	
+mb_convert_encoding($inStr,"UTF-8","ASCII");
 }
 include('flib.php');
 //где-то тут создаем массив $array[$i][$j] или читаем из файла (из базы данных), после чего начинаем формировать excel-файл
