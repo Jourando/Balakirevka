f5a6ca3bd71634ffe8384565e0a817461ab16bba
Paul the Stranger <paul.ivan.stranger@gmail.com>
2017-09-29 12:32:09 +0300

    adm tools 204 (import-export sequr fix)

--- www/scant.php
+++ www/scant.php
@@ -1,5 +1,5 @@
 <?php
-// v.10.a.0::scan revision
+// v.10.a.2::scan revision
 function getExtension($filename) {
 return substr($filename, strrpos($filename, '.') + 1);
 }
@@ -9,13 +9,12 @@ $handle = opendir($patch);
 while(($file = readdir($handle))) {
 	if (is_file($patch. DIRECTORY_SEPARATOR .$file)) {
 		if ($_POST['ftype']!='') {
-			// echo strtoupper(getExtension($file))." ::: ".strtoupper($_POST['ftype'])."<br>";
 			if (strtoupper(getExtension($file))==strtoupper($_POST['ftype'])) {
 				echo "<h5>Scan ".$patch. DIRECTORY_SEPARATOR .$file."</h5>";
 				$lines=file($patch. DIRECTORY_SEPARATOR .$file);
 				for ($i=0; $i<count($lines); $i++) {
 					if (strrpos($lines[$i], $_POST['SearchStr'])) {
-						echo ">><span style='color: red'>line ".$i." in ".$patch. DIRECTORY_SEPARATOR .$file."</span>: ".$_POST['SearchStr']." found<br>";
+						echo ">><span style='color: red'>line ".($i+1)." in ".$patch. DIRECTORY_SEPARATOR .$file."</span>: string '".$_POST['SearchStr']."' was found<br>";
 					}
 				}
 				echo "<hr>";
@@ -28,21 +27,41 @@ while(($file = readdir($handle))) {
 		renameDirAndFile($patch. DIRECTORY_SEPARATOR .$file);  // Обходим вложенный каталог
     }
 }
-closedir($handle);
-include "toolmen.php"; 
-}	
-
+closedir($handle); 
+}
 if ((!ISSET($_POST['SearchStr'])) || (trim($_POST['SearchStr'])=='')) {
 ?>
 <HTML>
 <HEAD><TITLE>SCAN</TITLE></HEAD>
 <BODY>
+<script>
+function substr( f_string, f_start, f_length ) {
+	// +	 original by: Martijn Wieringa
+	if(f_start < 0) {f_start += f_string.length;}
+	if(f_length == undefined) {
+		f_length = f_string.length;
+	} else if(f_length < 0){
+		f_length += f_string.length;
+	} else {
+		f_length += f_start;
+	}
+	if(f_length < f_start) {f_length = f_start;}
+	return f_string.substring(f_start, f_length);
+}
+function fx1() {
+var c = document.getElementById('s2').value;
+if (c.indexOf('.')>-1) {
+	document.getElementById('s2').value=substr(c, c.indexOf('.')+1);
+}
+document.mfrm.submit();
+}
+</script>
 <h3>fast text scan</h3>
-<FORM action="" method=post>
-	<input type=text name=SearchStr value=""> String<br>
-	<input type=text name=ftype value="php"> FileType<br>
-	<input type=text name=Dir value=""> Folder<br>
-	<input type=submit value=Scan>
+<FORM action="" name=mfrm method=post>
+	<input id=s1 type=text name=SearchStr value=""> String<br>
+	<input id=s2 type=text name=ftype value="php"> FileType<br>
+	<input id=s3 type=text name=Dir value=""> Folder<br>
+	<input type=button value=Scan Onclick=fx1()>
 </FORM>
 <? include "toolmen.php"; ?>
 </BODY>
@@ -50,6 +69,11 @@ if ((!ISSET($_POST['SearchStr'])) || (trim($_POST['SearchStr'])=='')) {
 <?
 } else {
 	$cur_dir = __DIR__;
+	if (ISSET($_POST['Dir'])) {
+		if (FILE_EXISTS($_POST['Dir'])) { $cur_dir=$_POST['Dir']; }
+	}
+	echo "Scan for '".$_POST['SearchStr']."' in *.".$_POST['ftype']." of ".$cur_dir;
 	renameDirAndFile($cur_dir);
+	include "toolmen.php";
 }
 ?>
