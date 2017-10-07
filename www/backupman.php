68c50d9c8e1dec5deb9278c2a075c5e70c3ee4c8
Paul the Stranger <paul.ivan.stranger@gmail.com>
2017-10-07 09:17:21 +0300

    remake main tab 21/adm tools 220 (10a)

--- www/backupman.php
+++ www/backupman.php
@@ -1,7 +1,21 @@
-<?php
+﻿<?php
 // v.10.a.2::backupman revision
-include('menu.php');
+// include('menu.php');
 echo "<h4>Выбирите раздел для отката</h4>";
-mkMenu();
+$xfile="depart0000.a";
+$lines = file($xfile, FILE_IGNORE_NEW_LINES | FILE_SKIP_EMPTY_LINES);
+$i=0;
+echo "<label>Отдел <select id=dps Onchange='depMod=this.selectedIndex'>\r\n";
+foreach($lines as $v) {
+	list($mVal[$i], $mLable[$i])=explode("=", $lines[$i]);
+	if ($i>0) {
+		$resStr[$i]="<option value=".$mVal[$i].">[".$mVal[$i]."] ".$mLable[$i]."</option>\r\n";
+	} else {
+		$resStr[$i]="<option value=".$mVal[$i].">".$mLable[$i]."</option>\r\n";
+	}
+	echo $resStr[$i];
+	$i=$i+1;
+}
+echo "</select></label>\r\n";
 // список файлов... директории <- дропдаун меню
 ?>
