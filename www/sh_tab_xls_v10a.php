d9ce4abfa1745da3957142bb493bfd8a5d733f1c
Paul the Stranger <paul.ivan.stranger@gmail.com>
2017-10-07 06:38:49 +0300

    remake main tab 15/adm tools 218 (10a)

--- www/sh_tab_xls_v10a.php
+++ www/sh_tab_xls_v10a.php
@@ -9,6 +9,7 @@ if (ISSET($_GET['us'])==true) {
 	$lx2='Фамилия и инициалы';
 	$lx3='Пароль';
 }
+include ('menu.php');
 ?>
 <!DOCTYPE html>
 <html lang="RU-ru">
@@ -323,38 +324,6 @@ var ResStr=act+delim+sect+delim+encodeURIComponent(oldStr)+delim+encodeURICompon
 postUrl(ResStr, xb, document.getElementById('hid').value);	
 }
 </script>
-<?
-function mkMenu() {
-$xfile="depart0000.a";
-$lines = file($xfile, FILE_IGNORE_NEW_LINES | FILE_SKIP_EMPTY_LINES);
-$i=0;
-echo "<form autocomplete=1><fieldset name=set id=fset>\r\n";
-echo "<label>Отдел <select id=dps Onchange='depMod=this.selectedIndex'>\r\n";
-foreach($lines as $v) {
-	list($mVal[$i], $mLable[$i])=explode("=", $lines[$i]);
-	if ($i>0) {
-		$resStr[$i]="<option value=".$mVal[$i].">[".$mVal[$i]."] ".$mLable[$i]."</option>\r\n";
-	} else {
-		$resStr[$i]="<option value=".$mVal[$i].">".$mLable[$i]."</option>\r\n";
-	}
-	echo $resStr[$i];
-	if ((!FILE_EXISTS('depart'.str_pad($i, 4, "0", STR_PAD_LEFT).'.a')) || (filesize('depart'.str_pad($i, 4, "0", STR_PAD_LEFT).'.a')<10)) {
-		if (FILE_EXISTS('store/depart')) {
-			copy('store/depart', 'depart'.str_pad($i, 4, "0", STR_PAD_LEFT).'.a');
-		} else {
-			$handle = fopen('depart'.str_pad($i, 4, "0", STR_PAD_LEFT).'.a', 'w');
-			fwrite($handle, " 0|||||||||||||||\r\n"); // менять!
-			fclose($handle);
-		}
-	}
-	$i=$i+1;
-}
-echo "</select></label>\r\n";
-echo "<label> оператор <input type=text id=lusr value=\"Фамилия и инициалы\" OnFocus=this.value=\"\" OnBlur=\"if (this.value=='') {this.value='Фамилия и инициалы';}\"> пароль <input id=pusr type=password value=Пароль OnFocus=this.value=\"\" OnBlur=\"if (this.value=='') {this.value='Пароль';}\"> <input type=button value=Отправить Onclick=\"Auth(1)\" class=visible id=b1> <input type=button value=Перелогиниться Onclick=\"Auth(2)\" class=invisible id=b2> <input type=button value=Обновить OnClick=\"location.reload()\"></label>\r\n";
-echo "</fieldset></form>\r\n";
-return $mLable;
-}
-?>
 <DIV id=mainSection>
 <? $tA=mkMenu() ?>
 <script>
