90b6e37e2550823aebafbbfad36f6b1021f7075e
Paul the Stranger <paul.ivan.stranger@gmail.com>
2017-10-09 05:20:42 +0300

    remake main tab 28/adm tools 227 (10a)

--- www/backupman.php
+++ www/backupman.php
@@ -12,11 +12,33 @@ $m1='';
 .cshow {position: absolute; top: 0; left: 0: z-index: 5; background: rgba(128, 128, 128, 0.6); width: 660px; height: 25px;}
 .chid{}
 </style>
+<?
+echo "<script>\r\n";
+echo "var Drp=new Array();\r\n";
+for ($i=1; $i<count($prE)+1; $i++) {
+	echo "var Dpr[".$i."]=new Array();\r\n";
+	$fdir = 'oldata/'.str_pad($i, 4, '0', STR_PAD_LEFT)."/";
+	// сканим через glob для вкатки в JS-array
+	$f = glob($fdir."*");
+// echo "В последний раз файл $filename был изменен: " . date ("F d Y H:i:s.", filemtime($filename));
+	for ($j=0; $j<count($f); $j++) {
+		echo "Dpr[".$i."][".$j."]='".$f[$j]." | ".date("Y m d H:i:s", filemtime($f[$j]))." | ".filesize($f[$j])."';\r\n";
+	}
+}
+echo "</script>\r\n";
+?>
 <script>
 var depMod=0;
 function xAct(a) {
-if (a>0) { document.getElementById("hs").className="hid"; document.getElementById("ds").className="chid";}
-else { document.getElementById("hs").className="show"; document.getElementById("ds").className="cshow";}
+if (a>0) {
+	document.getElementById("hs").className="hid";
+	document.getElementById("ds").className="chid";
+	for (i=1; i<Dpr.length; i++) {
+		for (j=0; j<Dpr[i].length; j++) {
+				document.getElementById('flist').innerHTML+='<div>'+Dpr[i][j]+'</div>';
+		}
+	}
+} else { document.getElementById("hs").className="show"; document.getElementById("ds").className="cshow";}
 }
 </script>
 </HEAD><BODY>
@@ -40,26 +62,11 @@ foreach($lines as $v) {
 }
 echo "</select></label>\r\n";
 echo "<br><br>\r\n";
-echo "<script>\r\n";
-echo "var Drp=new Array();\r\n";
-for ($i=1; $i<count($prE)+1; $i++) {
-	echo "var Dpr[".$i."]=new Array();\r\n";
-	$fdir = 'oldata/'.str_pad($i, 4, '0', STR_PAD_LEFT)."/";
-	// сканим через glob для вкатки в JS-array
-	$f = glob($fdir."*");
-// echo "В последний раз файл $filename был изменен: " . date ("F d Y H:i:s.", filemtime($filename));
-	for ($j=0; $j<count($f); $j++) {
-		echo "Dpr[".$i."][".$j."]='".$f[$j]." | ".date("Y m d H:i:s", filemtime($f[$j]))." | ".filesize($f[$j])."';\r\n";
-	}
-}
-// echo "Dpr[
-echo "</script>\r\n";
 echo "<h4>Доступные варианты:</h4>\r\n";
 echo "<div id=selrb style='position: relative;'>\r\n";
-echo "<div style='overflow: scroll; width: 650px; height: 350px; padding: 5px;'>&nbsp;</div>";
+echo "<div id=flist style='overflow: scroll; width: 650px; height: 350px; padding: 5px;'>&nbsp;</div>";
 echo "<div id=hs class=show> </div>\r\n";
 echo "</div>\r\n";
-echo "<!-- <input type=button value=1 onclick='document.getElementById(\"hs\").className=\"show\";'><input type=button value=2 onclick='document.getElementById(\"hs\").className=\"hid\";'> -->";
 echo "<div id=ctrl style='position: relative'><label><input type=button value=RollBack> to <input type=text readonly value=''></label>";
 echo "<div id=ds class=cshow> </div></div>";
 // список файлов... директории <- дропдаун меню
