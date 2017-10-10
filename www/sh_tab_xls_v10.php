ea6783d4938e7112155f55b00528196b083d3bbf
Paul the Stranger <paul.ivan.stranger@gmail.com>
2017-10-10 20:30:43 +0300

    remake main tab 63/adm tools 245 (CAPSLOCK+xls)

--- www/sh_tab_xls_v10.php
+++ www/sh_tab_xls_v10.php
@@ -289,7 +289,7 @@ foreach($lines as $v) {
 $ptmp=$ptmp."vxtp=vxtp+'</select></label>';\r\n";
 echo $ptmp;
 ?>
-	fst.innerHTML=vxtp+'<label> оператор <input type=text id=lusr value="Фамилия и инициалы" OnFocus=this.value="" OnBlur="if (this.value==\'\') {this.value=\'Фамилия и инициалы\';}"> пароль <input id=pusr type=password value=\'Пароль\' OnFocus=\'this.value=""; checkCapsWarning(event);\' OnBlur=\'if (this.value=="") {this.value="Пароль"}; removeCapsWarning();\' onkeyup=\'checkCapsWarning(event);\'> <div id=CLW class=CapsOff>&nbsp;</div> <input type=button value=Отправить Onclick=Auth(1) class=visible id=b1> <input type=button value=\'Перелогиниться\' Onclick=Auth(2) class=invisible id=b2> <input type=button value=Обновить OnClick="location.reload()"></label>';
+	fst.innerHTML=vxtp+'<label> оператор <input type=text id=lusr value="Фамилия и инициалы" OnFocus=this.value="" OnBlur="if (this.value==\'\') {this.value=\'Фамилия и инициалы\';}"> пароль <input id=pusr type=password value=\'Пароль\' OnFocus=\'this.value=""; checkCapsWarning(event);\' OnBlur=\'if (this.value=="") {this.value="Пароль"}; removeCapsWarning();\' onkeyup=\'checkCapsWarning(event);\'> <div id=CLW class=CapsOff ALT="проверьте CapsLock">&nbsp;</div> <input type=button value=Отправить Onclick=Auth(1) class=visible id=b1> <input type=button value=\'Перелогиниться\' Onclick=Auth(2) class=invisible id=b2> <input type=button value=Обновить OnClick="location.reload()"></label>';
 }	
 }
 function Prw() {
@@ -335,68 +335,49 @@ getContent('all', $tA); // первично считываем, заполняе
 ?>
 </Table>
 </DIV>
-  <script>
-    /**
-     * Текущее состояние CapsLock
-     *  - null : неизвестно
-     *  - true/false : CapsLock включен/выключен
-     */
-    var capsLockEnabled = null;
+<script>
+    /* Текущее состояние CapsLock
+	- null : неизвестно
+	- true/false : CapsLock включен/выключен
+	*/
+var capsLockEnabled = null;
 
-    function getChar(event) {
-      if (event.which == null) {
-        if (event.keyCode < 32) return null;
-        return String.fromCharCode(event.keyCode) // IE
-      }
-
-      if (event.which != 0 && event.charCode != 0) {
-        if (event.which < 32) return null;
-        return String.fromCharCode(event.which) // остальные
-      }
-
-      return null; // специальная клавиша
-    }
-
-    if (navigator.platform.substr(0, 3) != 'Mac') { // событие для CapsLock глючит под Mac
-      document.onkeydown = function(e) {
-        if (e.keyCode == 20 && capsLockEnabled !== null) {
-          capsLockEnabled = !capsLockEnabled;
-        }
-      }
-    }
-
-    document.onkeypress = function(e) {
-      e = e || event;
-
-      var chr = getChar(e);
-      if (!chr) return // special key
-
-      if (chr.toLowerCase() == chr.toUpperCase()) {
-        // символ, не зависящий от регистра, например пробел
-        // не может быть использован для определения CapsLock
-        return;
-      }
-
-      capsLockEnabled = (chr.toLowerCase() == chr && e.shiftKey) || (chr.toUpperCase() == chr && !e.shiftKey);
-    }
-
-
-    /**
-     * Проверить CapsLock
-     */
-    function checkCapsWarning() {
-		if (capsLockEnabled) {
-			document.getElementById('CLW').innerHTML="!";
-			document.getElementById('CLW').className='CapsOn';
-		} else {
-			document.getElementById('CLW').innerHTML="&nbsp;";
-			document.getElementById('CLW').className='CapsOff';
-		}
-    }
-    function removeCapsWarning() {
-      document.getElementById('CLW').innerHTML = '&nbsp;';
-	  document.getElementById('CLW').className='CapsOff';
-    }
-  </script>
+function getChar(event) {
+if (event.which == null) {
+	if (event.keyCode < 32) return null;
+	return String.fromCharCode(event.keyCode) // IE
+}
+if (event.which != 0 && event.charCode != 0) {
+	if (event.which < 32) return null;
+	return String.fromCharCode(event.which) // остальные
+}
+return null; // специальная клавиша
+}
+if (navigator.platform.substr(0, 3) != 'Mac') { // событие для CapsLock глючит под Mac
+	document.onkeydown = function(e) {
+		if (e.keyCode == 20 && capsLockEnabled !== null) {capsLockEnabled = !capsLockEnabled;}
+	}
+}
+document.onkeypress = function(e) {
+	e = e || event;
+	var chr = getChar(e);
+	if (!chr) return // special key
+	if (chr.toLowerCase() == chr.toUpperCase()) {return;} // символ, не зависящий от регистра, например пробел не может быть использован для определения CapsLock
+	capsLockEnabled = (chr.toLowerCase() == chr && e.shiftKey) || (chr.toUpperCase() == chr && !e.shiftKey);
+}
+function checkCapsWarning() {
+	if (capsLockEnabled) {
+		document.getElementById('CLW').innerHTML="!";
+		document.getElementById('CLW').className='CapsOn';
+	} else {
+		document.getElementById('CLW').innerHTML="&nbsp;";
+		document.getElementById('CLW').className='CapsOff';
+	}
+}
+function removeCapsWarning() {
+	document.getElementById('CLW').innerHTML = '&nbsp;';
+	document.getElementById('CLW').className='CapsOff';
+}
+</script>
 </body>
 </html>
