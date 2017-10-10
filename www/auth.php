771cf28b81b6fdfdb7af1ec95ae982059599cce0
Paul the Stranger <paul.ivan.stranger@gmail.com>
2017-10-10 04:45:08 +0300

    remake main tab 48/adm tools 245 (upd)

--- www/auth.php
+++ www/auth.php
@@ -1,32 +1,25 @@
 ﻿<?php
-// v.10.a.2::auth revision
+// v.10.a.3::auth revision
 $ResStr="";
 $xFrom="";
-// ввести парам. from...
-// если from существует, xFrom=$_GET['from'] иначе xFrom=default
 if (($_GET['act']=="R") || ($_GET['act']=="W") || ($_GET['act']=="D")) {
-// если переменные [departm], [log], [pwd] существуют, то
-// открыть список соответствий и сверить 1) пара log:pwd 2) пара log+pwd:departm 3) CRC(log+pwd+departm):CRC_data from file
-		echo '<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">';
-		echo '<html lang="ru"><head><meta charset="utf-8"><meta http-equiv="X-UA-Compatible" content="IE=edge"><title>Auth</title></head>';
+		echo "<!DOCTYPE html>\r\n";
+		echo "<html lang='RU-ru'>\r\n<head>\r\n<meta charset='utf-8'><meta http-equiv='X-UA-Compatible' content='IE=edge'>\r\n<title>Auth</title>\r\n</head>\r\n";
 	if ($_GET['act']=="R") {
 		$lines = file('username.a', FILE_IGNORE_NEW_LINES | FILE_SKIP_EMPTY_LINES);
 		$i=0;
-		echo "<body><p>Лист операторов/доступов к разделам</p>\n";
-		echo "<script>\nfunction addusr() {\nlocation.href='auth.php?act=W&d='+document.newusr.df.value+'&u='+document.newusr.lf.value+'&p='+document.newusr.pf.value+'&from=self';\n}\n</script>\n";
-		echo "<Table border=1>";
-		echo "<tr><td>действие</td><td>доступ к<br>разделу</td><td>пользователь</td></tr>";
+		echo "<body>\r\n<p>Лист операторов/доступов к разделам</p>\r\n";
+		echo "<script>\nfunction addusr() {\r\nlocation.href='auth.php?act=W&d='+document.newusr.df.value+'&u='+document.newusr.lf.value+'&p='+document.newusr.pf.value+'&from=self';\r\n}\r\n</script>\r\n";
+		echo "<Table border=1>\r\n";
+		echo "<tr><td>действие</td><td>доступ к<br>разделу</td><td>пользователь</td></tr>\r\n";
 		foreach($lines as $v) {
 			list($d[$i], $u[$i], $p[$i], $c[$i]) = explode(":", $v);
-			// echo "now: [d=".$d[$i].", u=".$u[$i].", p=".$p[$i].", c=".$c[$i]."]<br>";
-			// echo $v.'<br>';
-			// если p=display - вывести меню редактирования
-			echo "<tr><td><a href=# Onclick=location.href='auth.php?act=D&d=".$d[$i]."&u=".$u[$i]."&from=self' style='text-decoration: none'>[x]</a></td><td>".$d[$i]."</td><td>".$u[$i]."</td></tr>";
+			echo "<tr><td><a href=# Onclick=location.href='auth.php?act=D&d=".$d[$i]."&u=".$u[$i]."&from=self' style='text-decoration: none'>[x]</a></td><td>".$d[$i]."</td><td>".$u[$i]."</td></tr>\r\n";
 		}
-		echo "</Table><br><hr><br>";
-		echo "<form name=newusr>";
-		echo "<label>Добавить нового юзера:<br>Логин <input type=text name=lf> Пароль <input type=text name=pf> Раздел <input type=text name=df> <input type=button value=Сохранить Onclick=addusr()></label>";
-		echo "</form>";
+		echo "</Table>\r\n<br><hr><br>\r\n";
+		echo "<form name=newusr>\r\n";
+		echo "<label>Добавить нового юзера:<br>Логин <input type=text name=lf> Пароль <input type=text name=pf> Раздел <input type=text name=df> <input type=button value=Сохранить Onclick=addusr()></label>\r\n";
+		echo "</form>\r\n";
 		include('toolmen.php');
 	}
 	if ($_GET['act']=="W") {
@@ -49,9 +42,9 @@ if (($_GET['act']=="R") || ($_GET['act']=="W") || ($_GET['act']=="D")) {
 					$ResStr="#03: ERR"; //Не могу произвести запись в файл ($filename)";
 					exit;
 			} else {
-					$ResStr="<h3>Новый пользователь успешно добавлен</h3>\n";
+					$ResStr="<h3>Новый пользователь успешно добавлен</h3>\r\n";
 					if ($_GET['from']=='self') {
-						$ResStr=$ResStr."<script>\ndocument.write('<p>Подождите несколько секунд, база обновится</p>');\nsetTimeout(location.href='auth.php?act=R', 9000);\n</script>\n";
+						$ResStr=$ResStr."<script>\r\ndocument.write('<p>Подождите несколько секунд, база обновится</p>');\r\nsetTimeout(location.href='auth.php?act=R', 9000);\r\n</script>\r\n";
 					}
 			}
 			fclose($handle);
@@ -76,19 +69,18 @@ if (($_GET['act']=="R") || ($_GET['act']=="W") || ($_GET['act']=="D")) {
 		fclose($handle);
 		$ResStr="<h3>Пользователь ".strtoupper($_GET['u'])." успешно удален</h3>\n";
 		if ($_GET['from']=='self') {
-			$ResStr=$ResStr."<script>\ndocument.write('<p>Подождите несколько секунд, база обновится</p>');\nsetTimeout(location.href='auth.php?act=R', 9000);\n</script>\n";
+			$ResStr=$ResStr."<script>\r\ndocument.write('<p>Подождите несколько секунд, база обновится</p>');\r\nsetTimeout(location.href='auth.php?act=R', 9000);\r\n</script>\r\n";
 		}
 		echo $ResStr;
 	}
 	echo ""; // вставка интерфейса управления
-	echo '</body></html>';
+	echo "</body></html>\r\n";
 }
 if ($_GET['act']=="C") {
 	if (isset($_GET['d'], $_GET['u'], $_GET['p'])) {
 		$new_u=urldecode($_GET['u']);
 		$new_p=urldecode($_GET['p']);
 		$new_d=$_GET['d'];
-		// если p=user + u=login (ISSET) + d>0, вывести сид для юзера... позитивный, если доступ к разделу разрешен
 		if (($new_d<1) && (strtoupper($new_u) !== 'ADM') && (strtoupper($new_u) !== 'ADMIN')) { $ResStr='param error (d)'; }
 		else {
 				if ((strtoupper($new_u) == 'ADM') || (strtoupper($new_u) == 'ADMIN')) { 
@@ -101,13 +93,10 @@ if ($_GET['act']=="C") {
 				$lines = file('username.a', FILE_IGNORE_NEW_LINES | FILE_SKIP_EMPTY_LINES);	
 				foreach($lines as $v) {
 					list($d[$i], $u[$i], $p[$i], $c[$i]) = explode(":", $v);
-					if (($d[$i]==$new_d) && ($u[$i]==strtoupper($new_u)) && ($p[$i]==md5($new_p))) {
-						// значение раздела и авторизация
-						$ResStr="+".$d[$i];
-					}
+					if (($d[$i]==$new_d) && ($u[$i]==strtoupper($new_u)) && ($p[$i]==md5($new_p))) {$ResStr="+".$d[$i];}
 				}
 		}
-	} else { $ResStr='param error (general)'; }
+	} else {$ResStr='param error (general)';}
 	echo $ResStr;
 }
 ?>
