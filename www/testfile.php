2971e312be1b9c97cc5f7e06446bdbc03f3c2b32
Paul the Stranger <paul.ivan.stranger@gmail.com>
2017-10-11 07:04:30 +0300

    remake main tab 85/adm tools 253 (adm arch.zip test)

--- www/testfile.php
+++ www/testfile.php
@@ -1,11 +1,13 @@
 <?php
 include('linelim.php');
 $zip = new ZipArchive();
-$filename = "back".time().".zip";
+$filename = "oldata/LONG/back".time().".zip";
+$dt=localtime();
 if ($zip->open($filename, ZipArchive::CREATE)!==TRUE) {exit("Невозможно открыть <".$filename.">\n");}
 else {
 	for ($i=0; $i<$deplimit; $i++) {
 		$zip->addGlob('oldata/'.str_pad($i, 4, "0", STR_PAD_LEFT).'/*');
 	}
+	$zip->setArchiveComment('Файлы откатов, созданные до '.$dt['tm_year'].'/'.$dt['tm_mon'].'/'.$dt[tm_mday].' числа'); 
 	$zip->close();
 }
