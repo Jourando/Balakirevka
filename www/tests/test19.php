55bedf2eff171bac04a33439a7c04d23aac8be99
Paul the Stranger <paul.ivan.stranger@gmail.com>
2017-10-11 04:09:34 +0300

    remake main tab 74/adm tools 245 (xls+csv+a arch.zip TEST)

--- www/tests/test19.php
+++ www/tests/test19.php
@@ -3,13 +3,11 @@
 $zip = new ZipArchive();
 $filename = "test21_php.zip";
 
-if ($zip->open($filename, ZipArchive::CREATE)!==TRUE) {
-    exit("Невозможно открыть <$filename>\n");
-}
+if ($zip->open($filename, ZipArchive::CREATE)!==TRUE) {exit("Невозможно открыть <$filename>\n");}
 echo $thisdir."<hr>";
 $zip->addFromString("testfilephp.txt" . time(), "#1 Это тестовая строка, добавленная как testfilephp.txt.\n");
 $zip->addFromString("testfilephp2.txt" . time(), "#2 Это тестовая строка, добавленная как testfilephp2.txt.\n");
-$zip->addFile($thisdir . "test1.php","test2.php");
+$zip->addFile("test1.php","test2.php");
 echo "numfiles: " . $zip->numFiles . "\n";
 echo "status:" . $zip->status . "\n";
 $zip->close();
