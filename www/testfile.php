7254c02bc6e576fe449b242f5cb63d17bdbd1a00
Paul the Stranger <paul.ivan.stranger@gmail.com>
2017-10-11 10:00:09 +0300

    remake main tab 95/adm tools 255 (adm arch.zip)

--- www/testfile.php
+++ www/testfile.php
@@ -6,12 +6,16 @@ for ($i=1; $i<count($f); $i++) {
 	if (($f[$i]!=='.') && ($f[$i]!=='..')) {
 		if ($f[$i]!==$except) {
 			unlink($wdir.$f[$i]);
-//			echo "del ".$f[$i]."<br>"
+			echo "add ".$f[$i]." to arch, then remove<br>\r\n";
+		} else {
+			echo "add ".$f[$i]." to arch<br>\r\n";
 		}
+
 	}
 }
 }
 $tmpstr=date("l, Y.d.m H:i:s");
+if (!file_exists('oldata/LONG')) {mkdir('oldata/LONG', 0744, true);}
 $zip = new ZipArchive();
 $filename="oldata/LONG/back_".time().".zip";
 if ($zip->open($filename, ZipArchive::CREATE)!==TRUE) {exit("Невозможно открыть <".$filename.">\r\n");}
@@ -21,11 +25,13 @@ else {
 	}
 	$zip->setArchiveComment('Backup files, created before '.$tmpstr); 
 	$zip->close();
-	echo "Файлы, которые были включены в архив <a href=".$filename.">".$filename."</a>:\r\n";
+	echo "<div style='border: 1px solid #999; width: 540; background: #000; color: #fff'>Файлы, которые были включены в архив <a href=".$filename." style='color: gold'>".$filename."</a>:</div>\r\n";
+	echo "<div style='border: 1px solid #999; height: 320px; width: 540px; overflow: scroll;'>";
 	for ($j=0; $j<$deplimit; $j++) {
 		if (is_dir('oldata/'.str_pad($j, 4, "0", STR_PAD_LEFT))) {
 			killDir('oldata/'.str_pad($j, 4, "0", STR_PAD_LEFT).'/', 'depart'.str_pad($j, 4, "0", STR_PAD_LEFT).'[0001]');
 		}
 	}
+	echo "</div>\r\n";
 }
 ?>
