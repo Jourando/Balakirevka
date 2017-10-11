418da1b54eedaba6391544fac34fb27920cdae09
Paul the Stranger <paul.ivan.stranger@gmail.com>
2017-10-11 09:40:51 +0300

    remake main tab 92/adm tools 255 (adm arch.zip test)

--- www/testfile.php
+++ www/testfile.php
@@ -4,25 +4,28 @@ function killDir($wdir, $except) {
 $f=scandir($wdir);
 for ($i=1; $i<count($f); $i++) {
 	if (($f[$i]!=='.') && ($f[$i]!=='..')) {
-		if ($f[$i]!==$except) {unlink($wdir.$f[$i]); echo "del ".$f[$i]."<br>";}
+		if ($f[$i]!==$except) {
+			unlink($wdir.$f[$i]);
+//			echo "del ".$f[$i]."<br>"
+		}
 	}
 }
 }
 $tmpstr=date("l, Y.d.m H:i:s");
 $zip = new ZipArchive();
-$filename = "oldata/LONG/back_".time().".zip";
-if ($zip->open($filename, ZipArchive::CREATE)!==TRUE) {exit("Невозможно открыть <".$filename.">\n");}
+$filename="oldata/LONG/back_".time().".zip";
+if ($zip->open($filename, ZipArchive::CREATE)!==TRUE) {exit("Невозможно открыть <".$filename.">\r\n");}
 else {
 	for ($i=0; $i<$deplimit; $i++) {
 		$zip->addGlob('oldata/'.str_pad($i, 4, "0", STR_PAD_LEFT).'/*');
 	}
 	$zip->setArchiveComment('Backup files, created before '.$tmpstr); 
 	$zip->close();
-}
-echo $tmpstr;
-for ($j=0; $j<$deplimit; $j++) {
-	if (is_dir('oldata/'.str_pad($j, 4, "0", STR_PAD_LEFT))) {
-		killDir('oldata/'.str_pad($j, 4, "0", STR_PAD_LEFT).'/', 'depart'.str_pad($j, 4, "0", STR_PAD_LEFT).'[0001]');
+	echo "Файлы, которые были включены в архив <a href=".$filename.">".$filename."</a>:\r\n";
+	for ($j=0; $j<$deplimit; $j++) {
+		if (is_dir('oldata/'.str_pad($j, 4, "0", STR_PAD_LEFT))) {
+			killDir('oldata/'.str_pad($j, 4, "0", STR_PAD_LEFT).'/', 'depart'.str_pad($j, 4, "0", STR_PAD_LEFT).'[0001]');
+		}
 	}
 }
 ?>
