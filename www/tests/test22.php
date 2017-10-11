2f9a2dc469f5b9452360710f72095d5b95c0c37e
Paul the Stranger <paul.ivan.stranger@gmail.com>
2017-10-11 21:38:37 +0300

    remake main tab 97/adm tools 255 (adm arch.zip)

--- www/tests/test22.php
+++ www/tests/test22.php
@@ -1,7 +1,12 @@
 <?php
 // Пример #4 Пример использования Zip
-$zip = zip_open("test21_php.zip");
+$zip = zip_open("test21.zip");
 if ($zip) {
+	echo "<DIV>Архив " . $zip->filename ." содержит:<br>\r\n"
+	echo "Число доступных файлов: " . $zip->numFiles . "<br>\r\n";
+	echo "Статус/системный статус: " . $zip->status  ."/".$zip->statusSys. "<br>\r\n";
+	echo "Список комментариев: " . $zip->comment . "</DIV>\r\n";
+
 	while ($zip_entry = zip_read($zip)) {
 		echo "<PRE>";
 		echo "Название:         ".zip_entry_name($zip_entry)."\n";
