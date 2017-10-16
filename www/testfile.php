02680280ba79019243e98753a070976ea1ef2b14
Paul the Stranger <paul.ivan.stranger@gmail.com>
2017-10-16 19:02:12 +0300

    remake main tab 120/adm tools 261 (adm zip)

--- www/testfile.php
+++ www/testfile.php
@@ -6,6 +6,7 @@ $f=scandir($wdir);
 <script>
 function fcontx(b, a) {
 var addStr='';
+console.log(b);
 if (a==1) {addStr='div';}
 else if (a==2) {addStr='block';}
 else if (a==3) {(addStr='file')}
@@ -31,13 +32,13 @@ for ($i=1; $i<count($f); $i++) {
 		$zip = zip_open($xfile);
 		$za->open($xfile);
 		if ($zip) {
-			echo "<DIV id=n".$i."file style='display: none'>Архив [<a href=".$xfile." style='text-decoration: none'>".$xfile."</a>] содержит:<br>Внутреннее имя ресурса: ".$zip."<br>Дата создания: ".date("Y/m/d H:i:s", filemtime($xfile))."<br>Размер архива: ".filesize($xfile)." byte(s)<br>Число доступных файлов-записей: ".$za->numFiles."<br>Статус/системный статус: ".$za->status."/".$za->statusSys."<br>Список комментариев: ".$za->comment."<br>[<a href=# id=zip".$i." onclick='fcontx(this.id, 2)' style='text-decoration: none'>Подробнее</a>]</DIV>\r\n";
+			echo "<DIV id=n".$i."file style='display: none'>Архив [<a href=".$xfile." style='text-decoration: none'>".$xfile."</a>] содержит:<br>Внутреннее имя ресурса: ".$zip."<br>Дата создания: ".date("Y/m/d H:i:s", filemtime($xfile))."<br>Размер архива: ".filesize($xfile)." byte(s)<br>Число доступных файлов-записей: ".$za->numFiles."<br>Статус/системный статус: ".$za->status."/".$za->statusSys."<br>Список комментариев: ".$za->comment."<br>[<a href=# id=zip".$i." onclick='fcontx(this.id, 2)' style='text-decoration: none'>Подробнее</a>]\r\n";
 			$k=0;
 			echo "<div id=zip".$i."block style='display: none'>\r\n";
 			while ($zip_entry = zip_read($zip)) {					
 					$k=$k+1;
-					echo "<div style='width:462px; border: 1px solid #000;'><PRE style='font-weight: bold; width: 460px; background: lightyellow; margin: 0px; padding-left: 5px;'>\r\nНазвание:         ".zip_entry_name($zip_entry)."\r\nИсходный размер:  ".zip_entry_filesize($zip_entry)."\r\nСжатый размер:    ".zip_entry_compressedsize($zip_entry)."\r\nМетод сжатия:     ".zip_entry_compressionmethod($zip_entry)."\r\nСодержимое: [<a href=# id=xc".$k." onclick='fcontx(this.id, 1)' style='text-decoration: none'>Показать</a>]</PRE>";
-					echo "<DIV id=xc".$k."div style='width: 460px; height: 200px; overflow: scroll; background: lightblue; margin: 0px; border-bottom: 1px solid #333; display: none'>\r\n";
+					echo "<div style='width:462px; border: 1px solid #000;'><PRE style='font-weight: bold; width: 460px; background: lightyellow; margin: 0px; padding-left: 5px;'>\r\nНазвание:         ".zip_entry_name($zip_entry)."\r\nИсходный размер:  ".zip_entry_filesize($zip_entry)."\r\nСжатый размер:    ".zip_entry_compressedsize($zip_entry)."\r\nМетод сжатия:     ".zip_entry_compressionmethod($zip_entry)."\r\nСодержимое: [<a href=# id=xc".$i."x".$k." onclick='fcontx(this.id, 1)' style='text-decoration: none'>Показать</a>]</PRE>";
+					echo "<DIV id=xc".$i."x".$k."div style='width: 460px; height: 200px; overflow: scroll; background: lightblue; margin: 0px; border-bottom: 1px solid #333; display: none'>\r\n";
 					if (zip_entry_open($zip, $zip_entry, "r")) {
 						$buf = zip_entry_read($zip_entry, zip_entry_filesize($zip_entry));
 						echo $buf;
@@ -47,6 +48,7 @@ for ($i=1; $i<count($f); $i++) {
 			}
 			zip_close($zip);
 			echo "</div>\r\n<hr>\r\n";
+			echo "</DIV>\r\n";
 		}
 	}
 }		
